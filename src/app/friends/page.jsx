5683cb3ad677b9e6a50f85e8955dd789a5468b0c
killbyfire <killbyfiredev@gmail.com>
2023-08-08 16:50:12 +0200

    Comment update

--- src/app/friends/page.jsx
+++ src/app/friends/page.jsx
@@ -1,5 +1,5 @@
 // Add friend input with custom friend code
-// get friends by using .lk (like) and check if the current user id is in anyones friends array
+// get friends by using .like (like) and check if the current user id is in anyones friends array
 // custom nicknames stored like userid:name,userid:name
 
 export default async function FriendsPage() {
